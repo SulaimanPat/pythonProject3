3d74b44320bf17c79e3ad636d382166e0a415024
SulaimanPat <121177359+SulaimanPat@users.noreply.github.com>
2023-01-02 15:42:33 +0600

    Update win.py

--- venv/win.py
+++ venv/win.py
@@ -3,6 +3,16 @@ from decouple import config
 my_money=config('MY_MONEY', cast=int)
 def win(users_slot, bet_money):
     global my_money
+    try:
+        if users_slot > 31 or users_slot <= 0:
+            raise ValueError
+    except ValueError:
+        print("Введите число от 1 до 30")
+    try:
+        if bet_money > my_money:
+            raise ValueError
+    except ValueError:
+        print(f"Введите сумму не больше {my_money}")
     numbers=list(range(1,31))
     win_slot=randint(1, len(numbers)-1)
     print(f'Выиграшный слот под номером {win_slot}')
